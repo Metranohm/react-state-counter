4469ca9e77edc6758e3ba9c6f07d63d9e4922882
Metranohm <andrewwink@gmail.com>
2022-12-09 10:27:30 -0600

    add color functionality

--- src/Counter.js
+++ src/Counter.js
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [resetCount, setResetCount] = useState(0);
+  const [color, setColor] = useState('white');
 
   const handleIncrement = () => {
-    if (count === 3) {
-      handleReset()
-    } else {
-      setCount(count + 1)
-    }
+    count === 3 ? handleReset() : setCount(count + 1);
+    setColor('lightgreen');
   };
 
   const handleDecrement = () => {
-    setCount(count - 1)
+    count === -3 ? handleReset() : setCount(count - 1);
+    setColor('pink');
   };
 
   const handleReset = () => {
-    setCount(0)
+    setCount(0);
+    setResetCount(resetCount + 1);
   };
 
   return (
     <div>
-      <span>Current Count: {count}</span>
+      <span style={{ background:color }}>Current Count:{count}</span>
+      <span>Reset Count:{resetCount}</span>
       <section>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleDecrement}>-</button>
